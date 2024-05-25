acbacafa6214f89c105117ba4d5c1fafa1433970
polina27072 <fadpauls@yandex.ru>
2024-05-25 11:06:06 +0300

    res.send в controllers/categories.js /users.js

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ const sendUserCreated = (req, res) => {
 
 const sendUserUpdated = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end({ message: "Игра обновлена" });
+  res.send({ message: "Игра обновлена" });
 };
 
 const sendUserDeleted = (req, res) => {
